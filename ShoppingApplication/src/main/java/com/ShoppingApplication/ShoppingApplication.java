aee25d4c062495efd4a98e4ca6e1385c3f1568e3
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-03-08 19:05:01 +0530

    first commit

--- ShoppingApplication/src/main/java/com/ShoppingApplication/ShoppingApplication.java
+++ ShoppingApplication/src/main/java/com/ShoppingApplication/ShoppingApplication.java
@@ -1,12 +1,25 @@
 package com.ShoppingApplication;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ShoppingApplication {
 
+	public static  org.slf4j.Logger logger=LoggerFactory.getLogger(ShoppingApplication.class);
+	
+	@PostConstruct
+	public void intt()
+	{
+		logger.info("application started...");
+	}
 	public static void main(String[] args) {
+		
+		logger.info("application running........");
 		SpringApplication.run(ShoppingApplication.class, args);
 	}
 
--- ShoppingApplication/src/test/java/com/ShoppingApplication/ShoppingApplicationTests.java
+++ ShoppingApplication/src/test/java/com/ShoppingApplication/ShoppingApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ShoppingApplication;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShoppingApplicationTests {
+	
+	public static  org.slf4j.Logger logger=LoggerFactory.getLogger(ShoppingApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("test is running....");
+		assertEquals(true, true);
 	}
 
 }
